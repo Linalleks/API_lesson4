1fca0dee8f92f5cb99c9bbf883525956206858e9
Ангелина Петрова <linalleks@gmail.com>
2026-01-06 15:22:53 +0700

    publish photo to Telegram channel

--- telegramBot.py
+++ telegramBot.py
@@ -4,7 +4,11 @@ from decouple import config
 
 def main():
     bot = telegram.Bot(token=config('TG_BOT_TOKEN'))
-    bot.send_message(text='Тестовый Привет на канале "Космические фотки(т)"', chat_id='@space_photos_linalleks')
+    channel_id = '@space_photos_linalleks'
+    # bot.send_message(text='Тестовый Привет на канале "Космические фотки(т)"', chat_id=channel_id)
+    # bot.send_document(chat_id=channel_id, document=open('images/hubble.jpeg', 'rb'))
+    # bot.send_photo(chat_id=channel_id, photo=open('images/hubble.jpeg', 'rb'))
+    bot.send_photo(chat_id=channel_id, photo='https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg')
 
 
 if __name__ == '__main__':
