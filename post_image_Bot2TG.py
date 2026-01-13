3268dd467237bf7442efe7b95df6b788d8e949af
Ангелина Петрова <linalleks@gmail.com>
2026-01-13 22:16:35 +0700

    Added script post one image to TG-channel

---
+++ post_image_Bot2TG.py
@@ -0,0 +1,35 @@
+import telegram
+import os
+import random
+import argparse
+from decouple import config
+from helper_scripts import image_reduction
+from helper_scripts import get_all_files_paths
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--image_path', nargs='?')
+
+    return parser
+
+
+def main():
+    parser = create_parser()
+    bot = telegram.Bot(token=config('TG_BOT_TOKEN'))
+    channel_id = config('TG_CHANNEL_ID')
+    max_size = 10485760
+
+    if parser.parse_args().image_path:
+        image = parser.parse_args().image_path
+    else:
+        image = random.choice(get_all_files_paths('images'))
+
+    if os.path.getsize(image) > max_size:
+        new_image = image_reduction(image, max_size)
+        image = new_image
+    bot.send_photo(chat_id=channel_id, photo=open(image, 'rb'))
+
+
+if __name__ == '__main__':
+    main()
